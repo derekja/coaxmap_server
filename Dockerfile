b1c02ddc5aade226dfab44980d287a60b83baaf0
Ubuntu <ubuntu@root@algaeexplorer.ca>
2019-12-11 00:29:23 +0000

    pretty close, just working on getting axes right I think

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
-FROM node:10
+FROM node:10.17.0-buster
 EXPOSE 5000
 
 WORKDIR /usr/src/coaxmap_server
 
+# enable repositories and common packages
+RUN apt-get update
+#RUN echo deb http://archive.ubuntu.com/ubuntu eoan universe multiverse >> /etc/apt/sources.list; apt-get update; apt-get -y install software-properties-common build-essential; apt-get update
+
 # get dependencies
 COPY package*.json ./
 
-RUN sudo apt-get -y install libnetcdf-dev
+RUN apt-get -y install libnetcdf-dev
 
 RUN npm install
 
--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-const express = require("express");
+const express = require("express"),
+    slash   = require('express-slash');
 const app = express();
 var netcdf4 = require("netcdf4")
 const port = process.env.PORT || 5000;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Create the router using the same routing options as the app.
+app.enable('strict routing');
+var router = express.Router({
+    caseSensitive: app.get('case sensitive routing'),
+    strict       : app.get('strict routing')
+});
+
+app.use(router);
+app.use(slash());
 
 // create a GET route
-app.get("/express_backend", (req, res) => {
+router.get("/express_backend/", (req, res) => {
+  console.log(req);
+
   let yr = req.query.yr;
   let m = req.query.m;
   let d = req.query.d;
   let x = req.query.x;
   let y = req.query.y;
-  let fn = "../build/OLCI/" + yr + "/" + m + "/" + d + "/polymer/mosaic_output.nc";
-  let fh = new netcdf4.File(fn, "r");
-  let val = fh.root.variables['logchl'].readSlice(x,y);
+  let fn = "/OLCI/" + yr + "/" + m + "/" + d + "/polymer/mosaic_output.nc";
+  console.log("year: "+yr);
+  console.log(fn);
 
+  let fh = new netcdf4.File(fn, "r");
+  let val = fh.root.variables['logchl'].read(x,y);
 
   console.log(val);
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+
+  res.set('Content-Type', 'text/html');
+  res.send(val.toString());
 });
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));

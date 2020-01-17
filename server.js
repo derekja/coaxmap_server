a1a7bb53c91a5f4de854a03244fb260cd596647e
Ubuntu <ubuntu@root@algaeexplorer.ca>
2020-01-17 07:36:46 +0000

    working server.js. print statements not yet removed.

--- server.js
+++ server.js
@@ -16,24 +16,47 @@ app.use(slash());
 
 // create a GET route
 router.get("/express_backend/", (req, res) => {
-  console.log(req);
+ // console.log(req);
 
   let yr = req.query.yr;
   let m = req.query.m;
   let d = req.query.d;
-  let x = req.query.x;
-  let y = req.query.y;
+  let y = req.query.x;
+  let x = req.query.y;
   let fn = "/OLCI/" + yr + "/" + m + "/" + d + "/polymer/mosaic_output.nc";
-  console.log("year: "+yr);
-  console.log(fn);
+  //console.log("year: "+yr);
+  //console.log(fn);
 
   let fh = new netcdf4.File(fn, "r");
-  let val = fh.root.variables['logchl'].read(x,y);
+  let lchl = fh.root.variables['logchl'].read(x,y);
+  let algae = 10**(lchl);
+  let lat = fh.root.variables['lat'].read(x,y);
+  let lon = fh.root.variables['lon'].read(x,y);
 
-  console.log(val);
+     console.log('xy '+x+' '+y+' lat: '+lat+' lon: '+lon+' logchl '+lchl+' algae: '+algae);
 
-  res.set('Content-Type', 'text/html');
-  res.send(val.toString());
+
+//  if (algae=1) {algae=''};
+//  var i,j;
+//var tmpm = Array.from(Array(11), () => new Array(11));
+//var lattm = Array.from(Array(11), () => new Array(11));
+//var lontm = Array.from(Array(11), () => new Array(11));
+//  for (i=-5; i<6; i++) {
+//    for (j=-5; j<6; j++) {
+//     tmpm[i+5][j+5]=fh.root.variables['logchl'].read(parseInt(x)+i,parseInt(y)+j);
+//     lattm[i+5][j+5]=fh.root.variables['lat'].read(parseInt(x)+i,parseInt(y)+j);
+//     lontm[i+5][j+5]=fh.root.variables['lon'].read(parseInt(x)+i,parseInt(y)+j);
+//     console.log('ij '+i+' '+j+' lat: '+lattm[i+5][j+5]+' lon: '+lontm[i+5][j+5]+' logchl '+tmpm[i+5][j+5]);
+
+//    }
+//  }
+
+for (i=0; i<6; i++) {
+//console.log(tmpm[i]);
+}
+
+
+   res.json({lat: lat,lon: lon,algae: algae});
 });
 
 // console.log that your server is up and running
